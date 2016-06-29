e814d084571293dbd438ee6fa44ae9aef9d9c082
Tim De Pauw <tim.depauw@zentrick.com>
2016-06-29 20:11:19 +0200

    Remove unused chai-as-promised

--- test/lib/setup.js
+++ test/lib/setup.js
@@ -1,9 +1,7 @@
 import chai from 'chai'
-import chaiAsPromised from 'chai-as-promised'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 
-chai.use(chaiAsPromised)
 chai.use(sinonChai)
 
 global.expect = chai.expect
